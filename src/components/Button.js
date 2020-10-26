12d78331aa4f07f3fe99a9f15935318fd9cb1da5
JoshGrant5 <joshgg@icloud.com>
2020-10-26 15:18:34 -0700

    Added functionality for buttons

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,6 +2,21 @@ import React from "react";
 
 import "components/Button.scss";
 
+const classNames = require('classnames');
+
 export default function Button(props) {
-   
+  const buttonClass = classNames("button",{
+    "button--confirm": props.confirm,
+    "button--danger": props.danger
+  });
+
+  return (
+  <button 
+    disabled={props.disabled} 
+    onClick={props.onClick} 
+    className={buttonClass}
+  >
+    {props.children}
+  </button>
+  );
 }
