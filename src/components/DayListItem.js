83012a7acc1f73afc699c6405dbaec4b929a94b0
JoshGrant5 <joshgg@icloud.com>
2020-10-26 15:19:06 -0700

    Tests pass for buttons

---
+++ src/components/DayListItem.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "components/DayListItem.scss";
+
+const classNames = require('classnames');
+
+export default function DayListItem(props) {
+
+  const dayClass = classNames('day-list__item', {
+    'day-list__item--selected': props.selected,
+    'text--light': props.spots,
+    'day-list__item--full': props.spots === 0,
+    'text--regular': props.name
+  });
+
+  const formatSpots = spots => {
+    if (spots === 1) {
+      return `${spots} spot remaining`;
+    } else {
+      return (spots === 0 ? 'no spots remaining' : `${spots} spots remaining`);
+    }
+  };
+
+  return (
+    <li onClick={() => props.setDay(props.name)} >
+      <div className={dayClass}>
+        <h2>{props.name}</h2> 
+        <h3>{formatSpots(props.spots)}</h3>
+      </div>
+    </li>
+  );
+}
