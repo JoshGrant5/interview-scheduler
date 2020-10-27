4403642fcf9907fce403f4e5275e6c2bcf7c5690
JoshGrant5 <joshgg@icloud.com>
2020-10-27 14:19:26 -0700

    Edited the application component to include header, show, and empty components

--- src/components/Appointment/index.jsx
+++ src/components/Appointment/index.jsx
@@ -1,10 +1,24 @@
 import React from "react";
 import "components/Appointment/styles.scss";
+import Header from "components/Appointment/Header.jsx";
+import Show from "components/Appointment/Show.jsx";
+import Empty from "components/Appointment/Empty.jsx";
 
 export default function Appointment(props) {
-
   
   return (
-    <article className="appointment"></article>
+    <article className="appointment">
+      <Header
+        time={props.time}
+      />
+      {props.interview ? 
+      <Show
+        student={props.interview.student}
+        interviewer={props.interview.interviewer}
+      /> 
+      : 
+      <Empty />
+      }
+    </article>
   );
 }
