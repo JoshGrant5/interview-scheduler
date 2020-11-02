70c0c64eec51054984082ee55b2ec906f1286fb5
JoshGrant5 <joshgg@icloud.com>
2020-11-02 12:27:33 -0800

    Added prop-types for checking whether we are correctly passed an array as interviewer prop in interviewerList

--- src/components/InterviewerList.jsx
+++ src/components/InterviewerList.jsx
@@ -3,7 +3,9 @@ import "components/InterviewerList.scss";
 import InterviewerListItem from "components/InterviewerListItem";
 import "components/InterviewerListItem.scss";
 
-export default function InterviewerList(props) {
+const PropTypes = require('prop-types');
+
+const InterviewerList = props => {
 
   const interviewers = props.interviewers.map(interviewer => {
     return (
@@ -12,7 +14,7 @@ export default function InterviewerList(props) {
         name={interviewer.name}
         avatar={interviewer.avatar}
         selected={interviewer.id === props.value}
-        setInterviewer={event => props.onChange(interviewer.id)}
+        setInterviewer={() => props.onChange(interviewer.id)}
       />
     );
   });
@@ -24,3 +26,9 @@ export default function InterviewerList(props) {
     </section>
   );
 }
+
+InterviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired
+}
+
+export default InterviewerList;
