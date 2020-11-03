0631d34af80186eb4f42498d2e3fd6e630d6e613
JoshGrant5 <joshgg@icloud.com>
2020-11-03 13:23:29 -0800

    Removed reducer from useApplicationData into its own file and added a test for it

--- src/hooks/useApplicationData.jsx
+++ src/hooks/useApplicationData.jsx
@@ -1,59 +1,12 @@
 import { useReducer, useEffect } from "react";
 import axios from 'axios';
 
-const SET_DAY = "SET_DAY";
-const SET_APPLICATION_DATA = "SET_APPLICATION_DATA";
-const SET_INTERVIEW = "SET_INTERVIEW";
+import reducer, {
+  SET_DAY,
+  SET_APPLICATION_DATA,
+  SET_INTERVIEW
+} from "reducers/application";
 
-function reducer(state, action) {
-  switch (action.type) {
-    case SET_DAY:
-      return { ...state, day: action.day }
-    case SET_APPLICATION_DATA:
-      return { ...state, days: action.days, appointments: action.appointments, interviewers: action.interviewers }
-
-    case SET_INTERVIEW: {
-    
-      const interview = action.interview ? {...action.interview} : null;
-
-      const checkAppointment = {...state.appointments[action.id].interview};
-      let editOrDelete;
-      if (Object.keys(checkAppointment).length === 2) {
-        editOrDelete = true;
-      }
-
-      const appointment = {
-        ...state.appointments[action.id],
-        interview: interview
-      };
-      const appointments = {
-        ...state.appointments,
-        [action.id]: appointment
-      };
-      
-      const days = [...state.days];
-      let dayID;
-      let spots;
-      for (let day of days) {
-        if (day.appointments.includes(action.id)) {
-          if (editOrDelete) { 
-            // if interview is not null, we are editing. If null, we are deleting
-            interview ? [dayID, spots] = [day.id - 1, day.spots] : [dayID, spots] = [day.id - 1, day.spots + 1];
-          } else {
-            [dayID, spots] = [day.id - 1, day.spots - 1]; // Creating a new appointment (remove spot)
-          }
-        }
-      }
-      days[dayID] = {...days[dayID], spots: spots};
-
-      return { ...state, days, appointments }
-    }
-    default:
-      throw new Error(
-        `Tried to reduce with unsupported action type: ${action.type}`
-      );
-  }
-}
 const useApplicationData = () => {
   const [state, dispatch] = useReducer(reducer, {
     day: "Monday",
