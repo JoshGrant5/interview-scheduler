535e01b91486c6e17175c6953b22f41fdd9f1a57
JoshGrant5 <joshgg@icloud.com>
2020-11-03 16:15:14 -0800

    Throwing mock error to test in CircleCI

--- src/reducers/application.js
+++ src/reducers/application.js
@@ -1,11 +1,11 @@
-// const SET_DAY = "SET_DAY";
+const SET_DAY = "SET_DAY";
 const SET_APPLICATION_DATA = "SET_APPLICATION_DATA";
 const SET_INTERVIEW = "SET_INTERVIEW";
 
 export default function reducer(state, action) {
   switch (action.type) {
-    case SET_DAY:
-      return { ...state, day: action.day }
+    // case SET_DAY:
+    //   return { ...state, day: action.day }
 
     case SET_APPLICATION_DATA:
       return { ...state, days: action.days, appointments: action.appointments, interviewers: action.interviewers }
