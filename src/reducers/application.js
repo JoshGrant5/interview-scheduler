28d43ffb05afeb0db65e9b516e0473d6968b5ddf
JoshGrant5 <joshgg@icloud.com>
2020-11-03 16:17:39 -0800

    Fixed mock error for CircleCI

--- src/reducers/application.js
+++ src/reducers/application.js
@@ -4,8 +4,8 @@ const SET_INTERVIEW = "SET_INTERVIEW";
 
 export default function reducer(state, action) {
   switch (action.type) {
-    // case SET_DAY:
-    //   return { ...state, day: action.day }
+    case SET_DAY:
+      return { ...state, day: action.day }
 
     case SET_APPLICATION_DATA:
       return { ...state, days: action.days, appointments: action.appointments, interviewers: action.interviewers }
